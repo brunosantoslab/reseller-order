0920faaf2c281bb6a1463d7fbbc3374dfa6eca9f
GFT\bont <bruno.santos1>
2025-02-23 17:53:06 -0300

    [RESORD-2] Set up the initial project structure #2

---
+++ reseller-registration-service/src/main/java/app/brunosantos/resellerregistration/ResellerRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package app.brunosantos.resellerregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResellerRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResellerRegistrationServiceApplication.class, args);
+	}
+
+}
---
+++ reseller-registration-service/src/test/java/app/brunosantos/resellerregistration/ResellerRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.brunosantos.resellerregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResellerRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
